76fbda25d54ef15f75c183f8e30d998cc83096be
HaneenFarhat <haneenfarhat.33@gmail.com>
2022-02-01 17:35:33 +0200

    adding

---
+++ main.js
@@ -0,0 +1,79 @@
+
+// let num =77
+// let isOdd= num%2
+
+// let result =''
+
+
+
+
+
+
+
+
+// if (isOdd){
+//  result=(`${num} is an odd nunmber`)
+
+// } else{
+
+//     result=(`${num} is an even nunmber`)
+// }
+
+// console.log(result)
+
+
+
+
+// let num =1
+// let lessThenOne= 0
+// }
+
+// let result =''
+
+
+
+
+
+
+
+
+// if (lessThenOne){
+//  result=(` you have ${num} sandwich`)
+
+// } else{
+//     result=(` you have ${num} many sandwiches`)
+// }
+
+// console.log(result)
+
+
+
+let mark=90
+
+let result= ''
+
+
+if(mark>=90){ 
+    result=( `${mark} excellent`)
+
+
+}else if(mark>=80){
+
+    result=( `${mark} very Good`)
+
+    
+
+}else if (mark>=70){
+    result=( `${mark} good`)
+
+
+}else {
+    result=( ` ${mark} faild`)
+
+}
+
+
+
+
+  console.log(result);
+ 
